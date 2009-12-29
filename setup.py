289a676043c5645c53b93186a1a90ff55f16712f
collinw <collinw@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2009-12-29 22:08:21 +0000

    Use llvm-config to acquire flags from LLVM.
    This adds a new file, sysconfig, that gets installed and contains variables
    calculated at make-time.

--- setup.py
+++ setup.py
@@ -638,13 +638,10 @@ class PyBuildExt(build_ext):
 
         # LLVM wrappers
         if sysconfig.get_config_var("WITH_LLVM"):
-            (rel_llvm_inc_dir,) = sysconfig.get_config_vars('LLVM_INC_DIR')
-            llvm_inc_dir = os.path.join(srcdir, rel_llvm_inc_dir)
-            macros = [('__STDC_LIMIT_MACROS', 1), ('__STDC_CONSTANT_MACROS', 1)]
+            (llvm_cxxflags,) = sysconfig.get_config_vars('LLVM_CXXFLAGS')
             exts.append( Extension('_llvm', ['_llvm.cc'],
-                                   include_dirs=[rel_llvm_inc_dir,
-                                                 llvm_inc_dir],
-                                   define_macros=macros) )
+                                   # Yeah, this is intuitive. Go distutils.
+                                   extra_compile_args=llvm_cxxflags.split()) )
         else:
             missing.append('_llvm')
 
