7afaa7fe3559496699364d2be44c3467be90f128
collinw <collinw@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2010-01-27 00:18:58 +0000

    Add a separate release+symbols mode for install-llvm.sh.

--- Util/llvm/install-llvm.sh
+++ Util/llvm/install-llvm.sh
@@ -5,24 +5,30 @@
 #
 # ./install-llvm.sh debug --prefix=/tmp/llvm-dbg --other-configure-arg
 # ./install-llvm.sh release --prefix=/tmp/llvm-rel --some-other-option=foo
+# ./install-llvm.sh release+symbols --prefix=/tmp/llvm-syms
 #
 # debug == passing --with-pydebug to Python's ./configure
 # release == omitting --with-pydebug to Python's ./configure
+# release+symbols == like release, but with debug symbols
+#
+# The release mode is the closest to what will be shipped by package managers.
 #
 # You can then use Unladen Swallow's --with-llvm=/tmp/llvm-dbg option.
-# The {debug,release} option you pass to this script should match up with
-# whether you use --with-pydebug with ./configure.
+# The {debug,release,release+symbols} option you pass to this script should
+# match up with whether you use --with-pydebug with ./configure.
 
 # Keep these arguments in sync with the top-level configure.in.
 LLVM_ARGS="--enable-jit --enable-targets=host --enable-bindings=none"
 DEBUG_ARGS="--disable-optimized --enable-debug-runtime --enable-assertions"
-RELEASE_ARGS="--enable-optimized --disable-assertions --enable-debug-symbols"
+REL_SYMS_ARGS="--enable-optimized --disable-assertions --enable-debug-symbols"
+RELEASE_ARGS="--enable-optimized --disable-assertions --disable-debug-symbols"
 LLVM_DIR=`dirname $0`
 
 case "$1" in
     "debug") LLVM_ARGS="$LLVM_ARGS $DEBUG_ARGS";;
     "release") LLVM_ARGS="$LLVM_ARGS $RELEASE_ARGS";;
-    *) echo "First arg needs to be either 'debug' or 'release', not '$1'"; \
+    "release+symbols") LLVM_ARGS="$LLVM_ARGS $REL_SYMS_ARGS";;
+    *) echo "Invalid build type: '$1'"; \
        exit 1;;
 esac
 
